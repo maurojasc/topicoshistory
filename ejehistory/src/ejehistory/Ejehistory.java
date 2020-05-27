ee1e5927e77abdc0900f0b16c1fc6b2f9287ab19
Mauricio <Mauricio@DESKTOP-N1Q25V5>
2020-05-27 14:56:52 -0500

    Modificacion de MAuricio Rojas commit versión 2 desde método para history

--- ejehistory/src/ejehistory/Ejehistory.java
+++ ejehistory/src/ejehistory/Ejehistory.java
@@ -17,6 +17,9 @@ public class Ejehistory {
     public static void main(String[] args) {
         System.out.println("versión 0 para history");
         System.out.println("versión 1 adición de un sout");
+        imprimir();
     }
-    
+    static void imprimir(){
+        System.out.println("versión 2 desde método");
+    }
 }
